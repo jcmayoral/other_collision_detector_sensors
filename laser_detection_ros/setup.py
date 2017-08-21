d6980d945e844cd47f07115af70eaa920d2a9770
jcmayoral <jocamaba1989@gmail.com>
2017-08-21 15:26:43 +0200

    scripts running but cusum not displayed

--- laser_detection_ros/setup.py
+++ laser_detection_ros/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 d = generate_distutils_setup(
-    packages=['laser_detection_ros','fault_detection'],
+    packages=['laser_detection_ros','fault_detection_laser'],
     package_dir={'': 'src'}
 )
 
--- laser_detection_ros/src/fault_detection_laser/LaserGaussCUSUM.py
+++ laser_detection_ros/src/fault_detection_laser/LaserGaussCUSUM.py
@@ -1,26 +1,26 @@
 import rospy
 from MyStatics.RealTimePlotter import RealTimePlotter
+from MyStatics.GaussianPlotter import GaussPlot
+from FaultDetection import ChangeDetection
 from geometry_msgs.msg import AccelStamped
 import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
-from FaultDetection import ChangeDetection
 import matplotlib.pyplot as plt
 
 
-class LaserCUSUM(RealTimePlotter,ChangeDetection):
+class LaserGaussCUSUM(RealTimePlotter,ChangeDetection,GaussPlot):
     def __init__(self, max_samples = 500, pace = 2, cusum_window_size = 10 ):
         self.data_ = []
         self.data_.append([0,0,0])
-        self.step_ = []
-        self.step_.append(0)
         self.i = 0
         self.msg = 0
         self.window_size = cusum_window_size
         RealTimePlotter.__init__(self,max_samples,pace)
         ChangeDetection.__init__(self,10)
-        rospy.init_node("laser_detection_ros_cusum", anonymous=True)
-        rospy.Subscriber("accel", AccelStamped, self.accCB)
+        GaussPlot.__init__(self )
+        rospy.init_node("laser_detection_ros_gaus_cusum", anonymous=True)
+        rospy.Subscriber("scan_unified", AccelStamped, self.accCB)
         plt.legend()
         plt.show()
         rospy.spin()
@@ -28,7 +28,7 @@ class LaserCUSUM(RealTimePlotter,ChangeDetection):
 
     def accCB(self, msg):
         while (self.i< self.window_size):
-            self.addData([msg.accel.linear.x,msg.accel.linear.y, msg.accel.angular.z])
+            self.addData([i for i in msg.ranges])
             self.i = self.i+1
             if len(self.samples) is self.max_samples:
                 self.samples.pop(0)
@@ -36,7 +36,13 @@ class LaserCUSUM(RealTimePlotter,ChangeDetection):
         self.i=0
         self.changeDetection(len(self.samples))
         cur = np.array(self.cum_sum, dtype = object)
-        self.step_.append(self.msg)
-        self.data_.append(cur)
-        self.msg = self.msg + 1
-        self.update(msg.header.seq,self.step_,self.data_)
+        self.call(np.mean(self.samples, axis=0),np.var(self.samples, axis=0))
+        """
+        THIS IS NOT REALLY WORKING
+        x1 = np.linspace(-140, 140, len(self.s_z))
+        print(len(x1), len(np.sort(self.s_z)))
+        plt.scatter([x1,x1,x1],np.sort(self.s_z))
+        """
+        x = np.linspace(-140, 140, 200)
+        y = np.array([i.pdf(x) for i in self.rv])
+        self.update(msg.header.seq,x.tolist(),y.T.tolist())
