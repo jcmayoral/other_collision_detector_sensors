34b6f008846c558546ea9503c520e669b51a4e0b
jcmayoral <jocamaba1989@gmail.com>
2017-08-21 15:06:24 +0200

    laser_plot added

---
+++ laser_detection_ros/src/laser_detection_ros/Plotter.py
@@ -0,0 +1,24 @@
+import rospy
+from MyStatics.RealTimePlotter import RealTimePlotter
+from sensor_msgs.msg import LaserScan
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+class Plotter(RealTimePlotter):
+    def __init__(self, threshold = 1000, pace = 200):
+        self.data_ = []
+        self.step_ = []
+        print ("Plotter Constructor Initialized")
+        super().__init__(threshold,pace)
+        self.ax.legend("False")
+        rospy.init_node("laser_plotter", anonymous=True)
+        rospy.Subscriber("scan_unified", LaserScan, self.laserCB)
+        plt.show()
+        rospy.spin()
+        plt.close("all")
+
+    def laserCB(self, msg):
+        self.step_.append(msg.header.seq)
+        self.data_.append([i for i in msg.ranges])
+        self.update(msg.header.seq,self.step_,self.data_)
